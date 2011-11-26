20b4abec1ca123cdd41e5cdac3bc88601b7786ea
Patrick M. Elsen <pelsen.vn@gmail.com>
2011-11-26 12:42:22 +0100

    cleaned up in sl_list header

--- sl_list.h
+++ sl_list.h
@@ -26,74 +26,35 @@
 #ifndef SL_LIST_H
 #define SL_LIST_H
 
-#ifndef bool
-#define bool short
-#endif
-#ifndef true
-#define true 1
-#endif
-#ifndef false
-#define false 0
-#endif
-#define SL_LIST sizeof(sl_list)
-
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "list.h"
 
-
-// single lined list node
 struct sl_list_node
 {
-    // data pointer
     void *data;
-    // next list node
     struct sl_list_node *next;
 };
-
-// single linked list 'object'
-struct sl_list
-{
-    // pointer to first list element
-    struct sl_list_node *head;
-};
-
-typedef struct sl_list sl_list;
 typedef struct sl_list_node sl_list_node;
 
+struct sl_list
+{
+    sl_list_node *head;
+};
+typedef struct sl_list sl_list;
 
-// get the node of the list at pos
 sl_list_node *list_get_node(sl_list *list, size_t pos);
-
-// reset list nodes
+sl_list *sl_list_new();
 void list_reset(sl_list *list, bool free_data);
-
-// free list and all nodes (but no data pointers)
 void list_delete(sl_list *list, bool free_data);
-
-// append data to the end of the list
 void list_append(sl_list *list, void *data);
-
-// print diagnostics
 void list_debug(sl_list *list);
-
-// set an element to data
 void list_set(sl_list *list, size_t pos, void *data);
-
-// get the list's size
-size_t list_size(sl_list *list);
-
-// get the data stored at pos (or NULL)
 void *list_get(sl_list *list, size_t pos);
-
-// get the list's last element
 void *list_last(sl_list *list);
-
-// get the first element (or NULL)
 void *list_first(sl_list *list);
-
-// delete last list node and return data
 void *list_pop(sl_list *list);
+size_t list_size(sl_list *list);
 
 #endif
