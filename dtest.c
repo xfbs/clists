f28ec4ab66a84ccadde2e20f8d5c02501b661e77
Patrick M. Elsen <pelsen.vn@gmail.com>
2011-12-14 20:12:37 +0100

    wrote some dlist tests

--- dtest.c
+++ dtest.c
@@ -26,9 +26,37 @@ void dlist_debug(dlist *list)
 
 int main(void)
 {
+    void *data1 = (void*)0xDEADBEEF1;
+    void *data2 = (void*)0xCAFEBABE2;
+
+    printf("-> allocating new dlist\n");
     dlist *list = dlist_alloc();
     dlist_debug(list);
-    dlist_append(list, (void*)0xDEADBEEF);
+
+    printf("-> appending data to list\n");
+    dlist_append(list, data1);
     dlist_debug(list);
+
+    printf("-> copying list\n");
+    dlist *copy = dlist_copy(list);
+    dlist_debug(copy);
+
+    printf("-> joining lists\n");
+    dlist_merge(list, copy);
+    dlist_debug(list);
+
+    printf("-> adding to list\n");
+    dlist_append(copy, data2);
+    dlist_push(copy, data1);
+    assert(dlist_get(copy, 0) == data1);
+    assert(dlist_get(copy, 1) == data2);
+    dlist_debug(copy);
+
+    printf("-> popping from list\n");
+    assert(dlist_pop(copy) == data1);
+    dlist_debug(copy);
+
+    printf("-> freeing lists\n");
     dlist_free(list, 0);
+    dlist_free(copy, 0);
 }
