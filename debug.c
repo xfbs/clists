4f08dcd14b30377f2df9223c9134f68517046576
Patrick M. Elsen <pelsen.vn@gmail.com>
2011-12-14 20:11:01 +0100

    while it's not finished, I prefer to leave debug mode on

--- debug.c
+++ debug.c
@@ -4,12 +4,12 @@
 void *log_malloc(size_t size, const char *file, unsigned int line, const char *func)
 {
     void *data = malloc(size);
-    //printf("malloc(%3u) => %p [%s:%u in %s()]\n", (unsigned int)size, data, file, line, func);
+    printf("malloc(%3u) => %p [%s:%u in %s()]\n", (unsigned int)size, data, file, line, func);
     return data;
 }
 
 void log_free(void *data, const char *file, unsigned int line, const char *func)
 {
-    //printf("free(%p) [%s:%u in %s()]\n", data, file, line, func);
+    printf("free(%p) [%s:%u in %s()]\n", data, file, line, func);
     free(data);
 }
