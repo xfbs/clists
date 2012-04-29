1f402251f75a4e9416213b0956a7a65824dc3c39
Patrick M. Elsen <pelsen.vn@gmail.com>
2012-04-29 16:36:05 +0200

    expanded prepending tests for slist

--- tests/slist/prepending.c
+++ tests/slist/prepending.c
@@ -33,14 +33,27 @@ TEST(prependFull)
     ret = slist_prepend(list, (void*)0x1234);
     assertEquals(ret, 0);
 
-    ret = slist_prepend(list, (void*)0x4321);
+    ret = slist_prepend(list, (void*)0x2341);
     assertEquals(ret, 0);
 
     assertNotEquals(list->head, list->tail);
     assertEquals(list->size, 2);
     assertEquals(list->head->next, list->tail);
-    assertEquals(list->head->data, (void*)0x4321);
+    assertEquals(list->head->data, (void*)0x2341);
     assertEquals(list->tail->data, (void*)0x1234);
+
+    ret = slist_prepend(list, (void*)0x3412);
+    assertEquals(ret, 0);
+
+    assertEquals(list->size, 3);
+    assertNotEquals(list->head, NULL);
+    assertNotEquals(list->tail, NULL);
+    assertNotEquals(list->head->next, NULL);
+    assertNotEquals(list->head->next, NULL);
+    assertEquals(list->head->next->next, list->tail);
+    assertEquals(list->head->data, (void*)0x3412);
+    assertEquals(list->tail->data, (void*)0x1234);
+    assertEquals(list->head->next->data, (void*)0x2341);
 
     ret = slist_free(list);
     assertEquals(ret, 0);
