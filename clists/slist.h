935b482e4e913deb18a4907d61fd347fb91a15b9
Patrick M. Elsen <pelsen.vn@gmail.com>
2014-11-07 12:36:07 +0100

    added todo: conversion functions

--- clists/slist.h
+++ clists/slist.h
@@ -1,5 +1,5 @@
 /*  File: slist.h
- *  
+ *
  *  Copyright (C) 2011, Patrick M. Elsen
  *
  *  This file is part of CLists (http://github.com/xfbs/CLists)
@@ -20,6 +20,39 @@
  *  You should have received a copy of the GNU General Public License along
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  ==About==
+ *  SList is an implementation of a single linked list, in which
+ *  nodes have a pointer to the next node, thereby linking them.
+ *  Technically, you only need to know the position of the first
+ *  node to work with a single linked list, but in this implementation
+ *  single linked lists are represented with a dedicated data
+ *  structure (slist_t), which stores not only the first node,
+ *  but also the last node as well as the list size. This is done
+ *  for efficiency, as otherwise the whole list has to be traversed
+ *  to know it's size.
+ *
+ *  All functions that work with the SList data structure take a
+ *  pointer to an slist struct (slist_t*). Such a pointer can come
+ *  from the slist_new() function, which creates a new object on
+ *  the heap, initializes it and returns it's pointer, or an object
+ *  on the stack can be initialized with slist_init(&obj). See the
+ *  following examples:
+ *  
+ *      // create an slist on the heap (internally uses malloc)
+ *      slist_t *one = slist_new();
+ *
+ *      // create an slist on the stack and initialize it
+ *      slist_t two;
+ *      slist_init(&two);
+ *
+ *      // add some data to both lists
+ *      int num = 6;
+ *      slist_append(one,  (void*)&num);
+ *      slist_append(&two, (void*)&num);
+ *
+ *  For more information, see the comments above each function or
+ *  in the implementation.
  */
 
 #ifndef _SLIST_H
@@ -73,6 +106,10 @@ void *slist_pop(slist_t *list);
 
 int slist_equal(slist_t *lista, slist_t *listb);
 slist_t *slist_copy(slist_t *list);
+
+// [TODO] converstion functions
+//dlist_t *slist_to_dlist(slist_t *list);
+//pvec_t *slist_to_pvec(slist_t *list);
 void **slist_to_array(slist_t *list);
 
 #ifndef slist_size
